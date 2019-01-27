4fc09493e8d49c163436cc928f2f9b5c4565ae11
Blake <bwhite1195@yahoo.com>
2019-01-26 20:10:28 -0500

    added sticky footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 
-const Footer = props => {
-  return <div id="footer" />;
+const Footer = () => {
+  return (
+    <div id="footer">
+      <img src="/img/bitcoin-footer.png" />
+      <a href="https://bitcoin.org/en/#maindesc-title" target="_blank">
+        Offical Site
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
